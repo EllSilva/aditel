2d46d2da50186f8d5ebbccb95e91d622679b8ed3
unknown <joaquimmigueldecristo@gmail.com>
2023-02-02 14:18:30 -0300

    segundo commite 9

--- assets/js/view/energia/home.js
+++ assets/js/view/energia/home.js
@@ -3,50 +3,22 @@ import get_template from '../../components/get_template.js'
 export default {
     data: function () {
         return {
-            jms: "todos",
-            todos: true,
-            crecheX: false,
-            eventoX: false,
-            festaX: false,
-            portifolio: true
+            jms1: null,
+            jms2: null,
+            jms4: null,
+            jms3: null,
+            jms5: null,
+            jms6: null,
+            jms7: null,
+            jms8: null,
+            jms9: null, 
         }
     },
    
     
     
     methods: {
-
-        todas() {
-            this.jms = "todos",
-            this.todos = true,
-            this.crecheX = false,
-            this.eventoX = false,
-            this.festaX = false
-        },
-
-        creche() {
-            this.jms = "creche",
-              this.todos = false,
-                this.crecheX = true,
-                this.eventoX = false,
-                this.festaX = false
-        },
-
-        evento() {
-            this.jms = "evento",
-            this.todos = false,
-                this.crecheX = false,
-                this.eventoX = true,
-                this.festaX = false
-        },
-
-        festa() {
-            this.jms = "festa",
-            this.todos = false,
-                this.crecheX = false,
-                this.eventoX = false,
-                this.festaX = true
-        },
+ 
    
         
     },
